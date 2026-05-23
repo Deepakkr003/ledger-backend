ed0345a9bbe786283eb4a3cea324a1acd94ade18
Deepakkr003 <19deepak79@gmail.com>
2026-05-23 14:30:55 +0530

    bug fixed

--- src/controller/transaction.controller.js
+++ src/controller/transaction.controller.js
@@ -114,9 +114,6 @@ async function createTransactionController(req, res) {
       type: "DEBIT"
     } ], { session })
 
-    await (() => {
-        return new Promise((resolve) => setTimeout(resolve, 15 * 1000));
-    })()
 
     const creditLedgerEntry = await ledgerModel.create([ {
       account: toAccount,
